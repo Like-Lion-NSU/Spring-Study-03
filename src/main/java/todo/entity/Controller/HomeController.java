38a2b1ca347bbc6d83fa3d81ad295a1f40ded58e
안정현 <ajhajh1005@naver.com>
2023-05-22 16:24:08 +0900

    미완료

--- src/main/java/todo/entity/Controller/HomeController.java
+++ src/main/java/todo/entity/Controller/HomeController.java
@@ -3,27 +3,46 @@ package todo.entity.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import todo.entity.Dto.TodoEditRequestDto;
 import todo.entity.Dto.TodoSaveRequestDto;
 import todo.entity.Entity.Todo;
 import todo.entity.Service.TodoService;
+import todo.entity.Service.TodoServicelemp;
+
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/todo")
 public class HomeController {
     private TodoService service;
-
     @Autowired
-    HomeController(TodoServiceImp serviceImple) {
-        this.serviceImple = serviceImple;
+    HomeController(TodoServicelemp service) {
+        this.service = service;
+    }
+    @PostMapping()
+    public ResponseEntity<Todo> getTodo(@RequestBody TodoSaveRequestDto todoSaveDto, @RequestParam String id) {
+        Todo todo = service.saveTodo(id);
+    }
+    @GetMapping
+    public ResponseEntity<Todo> getTodoById(@PathVariable("id") Long id) {
+        Optional<Todo> todo = service.findById(id);
+        return todo.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping
-    public ResponseEntity<Todo> saveTodo(@RequestBody TodoSaveRequestDto todoSaveDto, @RequestParam String id) {
-        Todo todo = serviceImple.saveTodo(id);
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodoDoneStatus(
+            @RequestBody TodoEditRequestDto todoEditRequestDto, @PathVariable("id") Long id) {
+        Todo updatedTodo = service.updateDone(todoEditRequestDto, id);
+        if (updatedTodo != null) {
+            return ResponseEntity.ok(updatedTodo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-
-    @GetMapping("/")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Todo> deleteTodoById(@PathVariable("id") Long id) {
+        service.deleteTodo(id);
+        return ResponseEntity.noContent().build();
+    }
 }
